1bcf6568dae09d2efb434284eecda51efb93d7b7
Muad Mohamed <moadjini2@gmail.com>
2026-06-22 22:35:52 +0300

    Updated project

--- src/app/actions/platform.ts
+++ src/app/actions/platform.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createServiceClient } from "@/lib/supabase/server";
+import { createClient, createServiceClient } from "@/lib/supabase/server";
 import {
   sendTelegramNotification,
   notifyNewCampaign,
